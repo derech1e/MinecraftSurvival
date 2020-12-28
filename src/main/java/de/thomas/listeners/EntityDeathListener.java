bbf7d6dc0b6abf5821937ff74b291720466b20f7
Thomas Nürk <jojopaul88@gmail.com>
2020-12-28 16:43:40 +0100

    Fixed Custom ArmorStand Drops

---
+++ src/main/java/de/thomas/listeners/EntityDeathListener.java
@@ -0,0 +1,27 @@
+package de.thomas.listeners;
+
+import de.thomas.utils.builder.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class EntityDeathListener implements Listener {
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntityType() == EntityType.ARMOR_STAND) {
+            if (event.getDrops().size() != 0) {
+                event.getDrops().clear();
+                ArmorStand armorStand = (ArmorStand) event.getEntity();
+                event.getEntity().getWorld().dropItem(event.getEntity().getLocation(), new ItemBuilder(Material.ARMOR_STAND).setArmorStandSmall(armorStand.isSmall()).showArmorStandArms(armorStand.hasArms()).setName("§f" + generateArmorStandName(armorStand.isSmall(), armorStand.hasArms())).toItemStack());
+            }
+        }
+    }
+
+    private String generateArmorStandName(boolean isSmall, boolean hasArms) {
+        return isSmall & hasArms ? "Small Armor Stand with Arms" : isSmall & !hasArms ? "Small Armor Stand" : hasArms ? "Armor Stand with Arms" : "Armor Stand";
+    }
+}
