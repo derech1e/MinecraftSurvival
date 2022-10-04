46b0faeb2ab03fe5f380cdd9a690fd17b9ab2724
Thomas Nürk <thomas@nuerkler.de>
2022-10-04 18:06:58 +0200

    Defer world save message

--- src/main/java/de/thomas/listeners/WorldSaveListener.java
+++ src/main/java/de/thomas/listeners/WorldSaveListener.java
@@ -7,10 +7,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldSaveEvent;
 
+import java.util.Date;
+
 public class WorldSaveListener implements Listener {
+
+    private static double lastTime = -1;
 
     @EventHandler
     public void onSave(WorldSaveEvent event) {
-        Bukkit.broadcast(new Message(ChatColor.GRAY.toString() + ChatColor.ITALIC + "[Server: Saved the game]").getMessage());
+        if (event.getWorld().getName().equals("world") && (lastTime + 60000 * 30) <= new Date().getTime()) {
+            Bukkit.broadcast(new Message(ChatColor.GRAY.toString() + ChatColor.ITALIC + "[Server: Saved the game]").getMessage());
+            lastTime = new Date().getTime();
+        }
     }
 }
