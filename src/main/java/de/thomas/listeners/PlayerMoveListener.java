10feb6cda1dd8a78c273215d2a25ad82e5c84ac0
Thomas Nürk <thomas@nuerkler.de>
2022-10-04 18:06:43 +0200

    Added linear acceleration of player speed

--- src/main/java/de/thomas/listeners/PlayerMoveListener.java
+++ src/main/java/de/thomas/listeners/PlayerMoveListener.java
@@ -1,24 +1,75 @@
 package de.thomas.listeners;
 
+import de.thomas.minecraftsurvival.MinecraftSurvival;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 public class PlayerMoveListener implements Listener {
 
-    @EventHandler
+    private final List<Material> materials = Arrays.asList(Material.GRAY_CONCRETE, Material.LIGHT_GRAY_CONCRETE);
+    private final HashMap<UUID, Integer> accelerateIDs = new HashMap<>();
+    private final HashMap<UUID, Integer> slowDownIDs = new HashMap<>();
+
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
         Material prev = event.getFrom().clone().subtract(0, 1, 0).getBlock().getType();
         Material next = event.getTo().clone().subtract(0, 1, 0).getBlock().getType();
 
-        if (!event.hasExplicitlyChangedPosition() || prev == next || next.isAir()) {
+        if (!event.hasExplicitlyChangedPosition()) {
             return;
         }
-
-        switch (event.getTo().clone().subtract(0, 0.9, 0).getBlock().getType()) {
-            case GRAY_CONCRETE, LIGHT_GRAY_CONCRETE -> event.getPlayer().setWalkSpeed(1F);
-            default -> event.getPlayer().setWalkSpeed(0.2F);
+        if (next.isAir() || prev.isAir()) {
+            return;
         }
+        // Note: prev == next check does not work. Server cant process the high speed of the player
+
+        if (isStandingOnBlock(event.getTo().clone().subtract(0, 0.9, 0), materials)) {
+            removeTask(player, slowDownIDs);
+            if (!accelerateIDs.containsKey(player.getUniqueId()) && player.getWalkSpeed() != 1) {
+                accelerateIDs.put(player.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(MinecraftSurvival.getINSTANCE(), () -> {
+                    float newWalkSpeed = player.getWalkSpeed() < 0.9F ? player.getWalkSpeed() + 0.1F : 1F;
+                    player.setWalkSpeed(newWalkSpeed);
+                    if (player.getWalkSpeed() == 1F) {
+                        removeTask(player, accelerateIDs);
+                    }
+                }, 5, 1));
+            }
+        } else {
+            removeTask(player, accelerateIDs);
+            if (player.getWalkSpeed() != 0.2F && !slowDownIDs.containsKey(player.getUniqueId())) {
+                slowDownIDs.put(player.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(MinecraftSurvival.getINSTANCE(), () -> {
+                    float newWalkSpeed = player.getWalkSpeed() > 0.2F ? player.getWalkSpeed() - 0.1F : 0.2F;
+                    player.setWalkSpeed(newWalkSpeed);
+                    if (player.getWalkSpeed() == 0.2F) {
+                        removeTask(player, slowDownIDs);
+                    }
+                }, 5, 3));
+            }
+        }
+    }
+
+    private void removeTask(Player player, HashMap<UUID, Integer> taskIdMap) {
+        Bukkit.getScheduler().cancelTask(taskIdMap.getOrDefault(player.getUniqueId(), -1));
+        taskIdMap.remove(player.getUniqueId());
+    }
+
+    private boolean isStandingOnBlock(Location location, List<Material> materials) {
+        double boundXZ = 0.6000000238418579;
+        return materials.contains(location.getBlock().getType())
+                || materials.contains(location.clone().add(boundXZ, 0, 0).getBlock().getType())
+                || materials.contains(location.clone().add(0, 0, boundXZ).getBlock().getType())
+                || materials.contains(location.clone().add(-boundXZ, 0, 0).getBlock().getType());
     }
 }
