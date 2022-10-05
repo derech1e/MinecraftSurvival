4de57116dadb50ce44f9d92b590215d2078accf2
Thomas Nürk <thomas@nuerkler.de>
2022-10-05 17:38:33 +0200

    Send world saving message only to operators of the server

--- src/main/java/de/thomas/listeners/WorldSaveListener.java
+++ src/main/java/de/thomas/listeners/WorldSaveListener.java
@@ -3,6 +3,7 @@ package de.thomas.listeners;
 import de.thomas.utils.message.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldSaveEvent;
@@ -15,8 +16,8 @@ public class WorldSaveListener implements Listener {
 
     @EventHandler
     public void onSave(WorldSaveEvent event) {
-        if (event.getWorld().getName().equals("world") && (lastTime + 60000 * 30) <= new Date().getTime()) {
-            Bukkit.broadcast(new Message(ChatColor.GRAY.toString() + ChatColor.ITALIC + "[Server: Saved the game]").getMessage());
+        if (event.getWorld().getName().equals("world") && (lastTime + 60000 * 60) <= new Date().getTime()) {
+            Bukkit.getOnlinePlayers().stream().filter(Player::isOp).forEach(player -> new Message(ChatColor.GRAY.toString() + ChatColor.ITALIC + "[Server: Saved the game]").getMessage());
             lastTime = new Date().getTime();
         }
     }
